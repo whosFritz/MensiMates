683df141dd60c3b87155736359a6339e771bca18
Fritz Schubert <fritzschubert21@outlook.de>
2023-01-13 19:47:48 +0100

    changed title and home name

--- essensgetterapp/lib/main.dart
+++ essensgetterapp/lib/main.dart
@@ -20,11 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
+      title: "MensaApp",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePageWidget(title: "Counter App"),
+      home: const HomePageWidget(title: "Speiseplan"),
     );
   }
 }
