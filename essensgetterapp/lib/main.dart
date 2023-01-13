b789e2ac7aac8206edbe7d6f24af37b817054d63
Fritz Schubert <fritzschubert21@outlook.de>
2023-01-13 17:17:47 +0100

    Added Test Data for Dynmaic App

--- essensgetterapp/lib/main.dart
+++ essensgetterapp/lib/main.dart
@@ -16,7 +16,6 @@ class HomePageWidget extends StatefulWidget {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
