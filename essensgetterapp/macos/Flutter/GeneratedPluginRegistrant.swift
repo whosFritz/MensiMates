bdd910495965cbbe5a79f3d1674f630ec13d9513
Fritz Schubert <fritzschubert21@outlook.de>
2023-01-16 10:28:38 +0100

    modified:   essensgetterapp/pubspec.lock

--- essensgetterapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ essensgetterapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import audio_service
 import audio_session
 import audioplayers_darwin
+import flutter_local_notifications
 import path_provider_macos
 import sqflite
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
