1b5bee17e02fc2c632a6740c14bd56e6cf872157
Fritz Schubert <fritzschubert21@outlook.de>
2023-01-22 00:26:35 +0100

    added splash screen

--- essensgetterapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ essensgetterapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import audio_service
-import audio_session
-import audioplayers_darwin
 import flutter_local_notifications
-import path_provider_macos
 import shared_preferences_foundation
-import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
