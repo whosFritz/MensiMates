440ebde87ec1251dc6a9c43a3649371127d9abb1
Fritz Schubert <fritzschubert21@outlook.de>
2023-01-27 14:01:16 +0100

    odified:   essensgetterapp/lib/aboutpage_widget.dart

--- essensgetterapp/lib/aboutpage_widget.dart
+++ essensgetterapp/lib/aboutpage_widget.dart
@@ -150,7 +150,7 @@ class _AboutPageState extends State<AboutPage> {
                                     TextButton(
                                         onPressed: () {
                                           launchUrl(Uri.parse(
-                                              "https://github.com/whosFritz/Mensa-App/blob/master/essensgetterapp/impressum.md"));
+                                              "https://github.com/whosFritz/Mensa-App/blob/master/impressum.md"));
                                         },
                                         child: const Padding(
                                           padding: EdgeInsets.all(8.0),
@@ -159,7 +159,7 @@ class _AboutPageState extends State<AboutPage> {
                                     TextButton(
                                         onPressed: () {
                                           launchUrl(Uri.parse(
-                                              "https://github.com/whosFritz/Mensa-App/blob/master/essensgetterapp/privacy-policy.md"));
+                                              "https://github.com/whosFritz/Mensa-App/blob/master/privacy-policy.md"));
                                         },
                                         child: const Padding(
                                           padding: EdgeInsets.all(8.0),
