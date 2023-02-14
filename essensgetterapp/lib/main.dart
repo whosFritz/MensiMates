a61777da9a803935f223ef098015d0ae95844e27
Fritz Schubert <fritzschubert21@outlook.de>
2023-02-14 16:38:28 +0100

    not working well, maybe remove

--- essensgetterapp/lib/main.dart
+++ essensgetterapp/lib/main.dart
@@ -3,6 +3,11 @@ import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:intl/date_symbol_data_local.dart";
 import "package:flutter_neumorphic/flutter_neumorphic.dart";
+import "package:quick_actions/quick_actions.dart";
+
+import "mensi_class.dart";
+import "mensi_schedule.dart";
+import "navigation_drawer.dart";
 
 void main() {
   initializeDateFormatting("de_DE");
@@ -15,10 +20,44 @@ void main() {
   runApp(const MensiMates());
 }
 
-class MensiMates extends StatelessWidget {
+Mensi shortcutreturn(String type) {
+  for (final mensi in mensenliste) {
+    if (mensi.name == type) {
+      return mensi;
+    }
+  }
+  return Mensi(id: 0, name: "keine Mensa", oeffnungszeitenalles: ["immer"]);
+}
+
+class MensiMates extends StatefulWidget {
   const MensiMates({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MensiMates> createState() => _MensiMatesState();
+}
+
+class _MensiMatesState extends State<MensiMates> {
+  @override
+  void initState() {
+    super.initState();
+
+    const QuickActions quickActions = QuickActions();
+
+    quickActions.setShortcutItems(<ShortcutItem>[
+      for (final mensi in mensenliste)
+        ShortcutItem(
+            type: mensi.name, localizedTitle: mensi.name, icon: "launcher_icon")
+    ]);
+
+    quickActions.initialize((type) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  MensiSchedule(mensiobj: shortcutreturn(type))));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
