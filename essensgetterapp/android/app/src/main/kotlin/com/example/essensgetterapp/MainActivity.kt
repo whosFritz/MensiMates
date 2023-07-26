2ce7e29cb9a3537fdd869bf7633d8aa02afe4887
Fritz Schubert <fritzschubert21@outlook.de>
2023-07-26 11:48:53 +0200

    changed folder name from essensgetterapp to mensimates

--- essensgetterapp/android/app/src/main/kotlin/com/example/essensgetterapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.whosfritz.MensiMates
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ mensimates/android/app/src/main/kotlin/com/example/essensgetterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.whosfritz.MensiMates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
